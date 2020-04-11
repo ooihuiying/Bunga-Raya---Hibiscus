858996de7f43185332c728a53b43d9a2fbe1945d
ooihuiying <huang.ooihuiying@gmail.com>
2020-04-11 22:29:41 +0800

    restructure files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Title } from './public/components/Title'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Title />
         Hello, Welcome to Bunga-Raya-Hibiscus Website!
       </header>
     </div>
